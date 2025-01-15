47634dc08e6d06a4b526c876f5611b80128780cf
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2025-01-15 00:22:18 -0500

    Ensure autostart directory exists

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 cp ftc-scoring-display.py /home/pi/ftc-scoring-display.py
 chmod +x /home/pi/ftc-scoring-display.py
 
+mkdir -p /home/pi/.config/autostart
 cp ftc-scoring-display.desktop /home/pi/.config/autostart/ftc-scoring-display.desktop
